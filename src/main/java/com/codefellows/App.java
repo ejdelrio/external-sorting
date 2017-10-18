1e7d5d6dcad16f128b9c0f6eeb94e6cf4b8d8e1e
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-10-17 22:13:55 -0700

    Experimenting with lambdas :o

--- src/main/java/com/codefellows/App.java
+++ src/main/java/com/codefellows/App.java
@@ -10,14 +10,34 @@ import com.codefellows.FileSorter;
  */
 public class App
 {
+    interface LambdaTest {
+        void operation(String s);
+    }
+
+
+    public static void testLamda(String s, LambdaTest op) {
+        op.operation(s);
+    }
 
     public static void main( String[] args )
     {
+        LambdaTest printString = (String s) -> {
+            System.out.println(s);
+        };
+
+        LambdaTest printCapString = (String s) -> {
+            System.out.println(s.toUpperCase());
+        };
+
+
         FileSorter fs = new FileSorter(
                 "/Users/Eddie/codefellows/501/external-sorting/src/main/resources/test.txt",
                 "/Users/Eddie/codefellows/501/external-sorting/src/main/resources/result.txt"
         );
 
         fs.sortTestFile();
+
+        testLamda("test", printCapString);
+
     }
 }
