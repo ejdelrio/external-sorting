4f40555fb1fa09647301756c8408704c91c6d1b2
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-10-18 21:12:50 -0700

    Over wrote the deault compartor with a lambda expression

--- src/main/java/com/codefellows/FileSorter.java
+++ src/main/java/com/codefellows/FileSorter.java
@@ -3,19 +3,33 @@ package com.codefellows;
 import com.google.code.externalsorting.ExternalSort;
 import java.io.File;
 import java.io.IOException;
+import java.util.Comparator;
 
 public class FileSorter extends ExternalSort{
 
     private String inputFile;
     private String outputFile;
 
+    @FunctionalInterface
+    public interface Compare {
+        Comparator<String> op(String a, String b);
+    }
+
+
+
+
     public FileSorter(String inputFile, String outputFile) {
         super();
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+
+        this.defaultcomparator = (String a, String b) -> a.compareTo(b);
+
+
     }
 
     public void sortTestFile() {
+
         try {
             sort(
                     new File(inputFile),
@@ -24,8 +38,11 @@ public class FileSorter extends ExternalSort{
         } catch(IOException e) {
             e.getStackTrace();
         }
-
     }
+
+
+
+
 
 
 
